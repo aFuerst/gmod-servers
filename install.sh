e2980be89394e3234453de020e7239a09b2c1996
aFuerst <fuersta.2013@gmail.com>
2020-08-01 11:32:45 -0400

    Copy config into folders before starting server

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ tar -xvzf steamcmd_linux.tar.gz
 cd ..
 
 ./steamcmd/steamcmd.sh +login anonymous +force_install_dir ./gmod/server +app_update 4020 validate +quit
-#./steamcmd/steamcmd.sh +login anonymous +force_install_dir ./gmod/css +app_update 232330 validate +quit
+# ./steamcmd/steamcmd.sh +login anonymous +force_install_dir ./gmod/css +app_update 232330 validate +quit
--- run_ttt.sh
+++ run_ttt.sh
@@ -3,6 +3,11 @@
 key='9AA08A439BE56096E1A17138480CA365'
 collection='2183717817'
 game='terrortown'
-map='de_chateau'
+map='ttt_lego'
 game='garrysmod'
+
+cp ttt_cfg/config.txt ./steamcmd/gmod/server/garrysmod/data/mapvote
+cp ttt_cfg/custom.lua ./steamcmd/gmod/server/garrysmod/lua/autorun
+cp ttt_cfg/server.cfg ./steamcmd/gmod/server/garrysmod/cfg
+
 ./steamcmd/gmod/server/srcds_run -console -game $game +gamemode $game +map $map +maxplayers 10 -authkey $key +host_workshop_collection $collection
